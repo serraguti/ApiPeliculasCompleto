e18fceeca6231616e724659bc3f5a2740306c43b
Serra <pacoserranox@gmail.com>
2022-02-08 18:19:26 +0100

    commit

---
+++ ApiPeliculasCompleto/Models/Cliente.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiPeliculasCompleto.Models
+{
+    [Table("CLIENTES")]
+    public class Cliente
+    {
+        [Key]
+        [Column("IDCLIENTE")]
+        public int IdCliente { get; set; }
+        [Column("NOMBRE")]
+        public String Nombre { get; set; }
+        [Column("EMAIL")]
+        public string Email { get; set; }
+        [Column("PAGINAWEB")]
+        public string PaginaWeb { get; set; }
+        [Column("IMAGEN")]
+        public string Imagen { get; set; }
+    }
+}
---
+++ ApiPeliculasCompleto/Models/Genero.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiPeliculasCompleto.Models
+{
+    [Table("GENERO")]
+    public class Genero
+    {
+        [Key]
+        [Column("IDGENERO")]
+        public int IdGenero { get; set; }
+        [Column("GENERO")]
+        public string NombreGenero { get; set; }
+    }
+}
