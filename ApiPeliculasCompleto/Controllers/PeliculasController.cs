98c175e713265b5cbf3939f835e2fbe4c11270fb
Serra <pacoserranox@gmail.com>
2022-02-08 18:49:35 +0100

    commit

---
+++ ApiPeliculasCompleto/Controllers/PeliculasController.cs
@@ -0,0 +1,71 @@
+﻿using ApiPeliculasCompleto.Models;
+using ApiPeliculasCompleto.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiPeliculasCompleto.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PeliculasController : ControllerBase
+    {
+        private RepositoryPeliculas repo;
+
+        public PeliculasController(RepositoryPeliculas repo)
+        {
+            this.repo = repo;
+        }
+
+        [HttpGet]
+        public List<Pelicula> GetPeliculas()
+        {
+            return this.repo.GetPeliculas();
+        }
+
+        [HttpGet("{id}")]
+        public Pelicula FindPelicula(int id)
+        {
+            return this.repo.FindPelicula(id);
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public List<Genero> Generos()
+        {
+            return this.repo.GetGeneros();
+        }
+
+        [HttpGet]
+        [Route("[action]/{idcliente}")]
+        public List<PedidosCliente> PedidosCliente(int idcliente)
+        {
+            return this.repo.GetPedidoCliente(idcliente);
+        }
+
+        [HttpGet]
+        [Route("[action]/{idgenero}")]
+        public List<Pelicula> PeliculasGenero(int idgenero)
+        {
+            return this.repo.GetPeliculasGenero(idgenero);
+        }
+
+        [HttpGet]
+        [Route("[action]/{idcliente}")]
+        public Cliente Cliente(int idcliente)
+        {
+            return this.repo.FindCliente(idcliente);
+        }
+
+        [HttpPost]
+        [Route("[action]")]
+        public void AddPedido(Pedido pedido)
+        {
+            this.repo.AddPedido(pedido.IdCliente, pedido.IdPelicula, pedido.Cantidad
+                , pedido.Fecha, pedido.Precio);
+        }
+    }
+}
--- ApiPeliculasCompleto/Repositories/RepositoryPeliculas.cs
+++ ApiPeliculasCompleto/Repositories/RepositoryPeliculas.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ApiPeliculasCompleto.Data;
+using ApiPeliculasCompleto.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +9,69 @@ namespace ApiPeliculasCompleto.Repositories
 {
     public class RepositoryPeliculas
     {
+        private PeliculasContext context;
+
+        public RepositoryPeliculas(PeliculasContext context)
+        {
+            this.context = context;
+        }
+
+        public List<Genero> GetGeneros()
+        {
+            return this.context.Generos.ToList();
+        }
+
+        public List<Pelicula> GetPeliculas()
+        {
+            return this.context.Peliculas.ToList();
+        }
+
+        public List<Pelicula> GetPeliculasGenero(int idgenero)
+        {
+            var consulta = from datos in this.context.Peliculas
+                           where datos.IdGenero == idgenero
+                           select datos;
+            return consulta.ToList();
+        }
+
+        public Pelicula FindPelicula(int id)
+        {
+            return this.context.Peliculas.SingleOrDefault(z => z.IdPelicula == id);
+        }
+
+        public Cliente FindCliente(int id)
+        {
+            return this.context.Clientes.SingleOrDefault(x => x.IdCliente == id);
+        }
+
+        public List<PedidosCliente> GetPedidoCliente(int idcliente)
+        {
+            var consulta = from datos in this.context.PedidosClientes
+                           where datos.IdCliente == idcliente
+                           select datos;
+            return consulta.ToList();
+        }
+
+        public Cliente ExisteCliente(string email, int idcliente)
+        {
+            var consulta = from datos in this.context.Clientes
+                           where datos.Email == email
+                           && datos.IdCliente == idcliente
+                           select datos;
+            return consulta.FirstOrDefault();
+        }
+
+        public void AddPedido(int idcliente, int idpelicula
+            , int cantidad, DateTime fecha, int precio)
+        {
+            Pedido pedido = new Pedido();
+            pedido.IdCliente = idcliente;
+            pedido.IdPelicula = idpelicula;
+            pedido.Cantidad = cantidad;
+            pedido.Fecha = fecha;
+            pedido.Precio = precio;
+            this.context.Pedidos.Add(pedido);
+            this.context.SaveChanges();
+        }
     }
 }
