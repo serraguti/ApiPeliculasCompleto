a86f97c89f50f263c2be4ccab14736afe118b5ba
Serra <pacoserranox@gmail.com>
2022-02-08 18:27:53 +0100

    commit

---
+++ ApiPeliculasCompleto/Data/PeliculasContext.cs
@@ -0,0 +1,28 @@
+﻿using ApiPeliculasCompleto.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiPeliculasCompleto.Data
+{
+    public class PeliculasContext: DbContext
+    {
+        public PeliculasContext
+            (DbContextOptions<PeliculasContext> options)
+            :base (options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Pedido>()
+                .HasKey(z => new { z.IdCliente, z.IdPelicula });
+        }
+
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<PedidosCliente> PedidosClientes { get; set; }
+        public DbSet<Pelicula> Peliculas { get; set; }
+        public DbSet<Genero> Generos { get; set; }
+    }
+}
